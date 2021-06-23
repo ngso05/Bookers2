500a0f4247186de8b20a648de5bac1e87e6a5dcc
EC2 Default User <ec2-user@ip-172-31-80-208.ec2.internal>
2021-06-23 13:19:36 +0000

    [Update]最終調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '88e1dd92edfca01af026bf680d69467b02340dfc01e434f670e100eeb2e87a593890f699338da9836dea18a741169763ceb28d157ce3be0a677fc2f7c8ab67a9'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  devise_for :users
+  root to: 'homes#top'
+
+  get "/home/about" => "homes#about"
+  get 'users' => 'users#index'
+  resources :users,only:[:show,:edit,:update]
+  resources :books
 end
